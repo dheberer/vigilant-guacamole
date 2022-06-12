971e7f1a08fc57c6cdd8b185126c0d6262453f85
Dave Heberer <dave@coda.io>
2022-06-12 16:54:18 -0700

    adds album and artist fetching

--- music_utils.py
+++ music_utils.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 import os
+from mutagen.easyid3 import EasyID3
 
 def files_in_dir(path: str):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
+def _get_unique_tags(path: str, tag: str):
+  files = files_in_dir(path)
+  tags = set()
 
-def get_artists(path: str):
-  return []
+  for f in files:
+    if not f.endswith('.mp3'):
+      info = EasyID3(f)
+      data = info[tag]
+      if data == '' or data is None:
+        data = 'Unknown'
+      tags.add(data)
+  return list(tags)
+
+def get_album_artists(path: str):
+  return _get_unique_tags(path, 'albumartist')
 
 def get_albums(path: str):
-  return []
+  return _get_unique_tags(path, 'album')
 
 def create_dirs(path: str, dirs):
   pass
@@ -32,4 +45,10 @@ def simple_deduplicate(path: str):
     print('Removing ' + f)
     os.remove(f)
 
+def create_artist_dirs(scan_path: str, dest_path) -> None:
+  """
+  This will look for all mp3 files in the scan_path recursively
+  and build a list of unique names. Then it will write each of
+  the found artists in the dest_path
+  """
 
